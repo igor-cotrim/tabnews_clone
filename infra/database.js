b08f2416a66224b9d025eb2d630881dc5ebbaccb
igor-cotrim <igorcotrim.dev@gmail.com>
2025-11-13 20:26:16 -0300

    feat: create migrations api and tests

--- infra/database.js
+++ infra/database.js
@@ -26,7 +26,7 @@ function getSSLValues() {
     return { ca: process.env.POSTGRES_CA };
   }
 
-  return process.env.NODE_ENV === "development" ? false : true;
+  return process.env.NODE_ENV === "production" ? true : false;
 }
 
 export default {

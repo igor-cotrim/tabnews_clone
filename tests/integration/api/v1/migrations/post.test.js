23cf9b8c12cf1c199ea2204879d6d4826705ed21
igor-cotrim <igorcotrim.dev@gmail.com>
2025-11-26 20:28:38 -0300

    refactor: create clearDatabase function in tests

--- tests/integration/api/v1/migrations/post.test.js
+++ tests/integration/api/v1/migrations/post.test.js
@@ -1,10 +1,9 @@
-import database from "infra/database.js";
 import orchestrator from "tests/orchestrator";
 
 describe("POST /api/v1/migrations", () => {
   beforeAll(async () => {
     await orchestrator.waitForAllServices();
-    await database.query("drop schema public cascade; create schema public;");
+    await orchestrator.clearDatabase();
   });
 
   describe("Anonymous user", () => {
--- tests/orchestrator.js
+++ tests/orchestrator.js
@@ -1,5 +1,7 @@
 import retry from "async-retry";
 
+import database from "infra/database";
+
 async function waitForAllServices() {
   await waitForWebService();
 
@@ -19,8 +21,13 @@ async function waitForAllServices() {
   }
 }
 
+async function clearDatabase() {
+  await database.query("drop schema public cascade; create schema public;");
+}
+
 const orchestrator = {
   waitForAllServices,
+  clearDatabase,
 };
 
 export default orchestrator;

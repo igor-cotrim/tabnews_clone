81664e13c2f48a102bed3ae99eeef1ff23ef64bc
igor-cotrim <igorcotrim.dev@gmail.com>
2025-11-26 20:25:03 -0300

    test: rename all descriptions to use new pattern

--- tests/integration/api/v1/migrations/get.test.js
+++ tests/integration/api/v1/migrations/get.test.js
@@ -1,17 +1,21 @@
 import database from "infra/database.js";
 import orchestrator from "tests/orchestrator";
 
-beforeAll(async () => {
-  await orchestrator.waitForAllServices();
-  await database.query("drop schema public cascade; create schema public;");
-});
+describe("GET /api/v1/migrations", () => {
+  beforeAll(async () => {
+    await orchestrator.waitForAllServices();
+    await database.query("drop schema public cascade; create schema public;");
+  });
 
-test("GET to /api/v1/migrations should return 200", async () => {
-  const response = await fetch("http://localhost:3000/api/v1/migrations");
-  const responseBody = await response.json();
+  describe("Anonymous user", () => {
+    it("Running pending migrations", async () => {
+      const res = await fetch("http://localhost:3000/api/v1/migrations");
+      const body = await res.json();
 
-  expect(response.status).toBe(200);
+      expect(res.status).toBe(200);
 
-  expect(Array.isArray(responseBody)).toBe(true);
-  expect(responseBody.length).toBeGreaterThan(0);
+      expect(Array.isArray(body)).toBe(true);
+      expect(body.length).toBeGreaterThan(0);
+    });
+  });
 });
